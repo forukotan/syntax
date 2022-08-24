672c4e94682630f7b7fd6c069e9586e3efdab29c
forukotan <forukotan1@gmail.com>
2022-08-24 16:52:02 -0500

    syntax1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response;
+           var equal = (answer < 9) ? response = $"{answer} is less than nine " : response = $"{ answer} greater than or equal to nine ";
+            Console.WriteLine($"{response}");
+
+        }
+    }
+}
